b4bc284813ccdbed42d5e5f1b5f1929c6651eef1
dev-vikram <vikram.s@apiplatform.io>
2022-05-12 16:12:14 +0530

    Added putjob data_access_api

---
+++ dev-vikram/dev-vikram/DATA_ACCESS_API/PYTHON/putjob_v1.py
@@ -0,0 +1,12 @@
+import http.client
+import mimetypes
+conn = http.client.HTTPSConnection("")
+payload = ''
+headers = {'pkey' : '3fbbb8bc5a969f503fdb66e7d90509d6'
+'apikey' : '7xR2sYhqRAdfhhyu6jMo9E9hi4fRazuw'
+'Content-Type' : 'application/json'
+}
+conn.request("PUT", "https://dev-vikram.gateway.apiplatform.io/v1/job/:id", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
