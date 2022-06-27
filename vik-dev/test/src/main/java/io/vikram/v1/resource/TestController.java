afb2f9bcdaef70dabdbc7380e65048c0ae46c9fc
dev-vikram <vikram.s@apiplatform.io>
2022-06-27 16:44:02 +0530

    commiting test to github

---
+++ vik-dev/test/src/main/java/io/vikram/v1/resource/TestController.java
@@ -0,0 +1,54 @@
+package io.vikram.v1.resource;
+
+import io.vikram.v1.model.Test;
+import io.vikram.v1.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestRepository repository;
+
+    @GetMapping("/test")
+    public List<Test> getTestData(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/test")
+    public String saveTest(@RequestBody Test test) {
+        repository.save(test);
+        return "Added test data";
+    }
+
+    @GetMapping("/test/{id}")
+    public Optional<Test> getTest(@PathVariable String id) {
+        return repository.findById(id);
+    }
+
+    @DeleteMapping("/test/{id}")
+    public String deleteTest(@PathVariable String id) {
+        repository.deleteById(id);
+        return "test deleted with id : " + id;
+    }
+
+    @PutMapping("/test/{id}")
+    public ResponseEntity<Test> updateTest(@PathVariable String id, @RequestBody Test test) {
+        Optional<Test> testData = repository.findById(id);
+        if (testData.isPresent()) {
+            Test _test = testData.get();
+            _test.setData(test.getData());
+            
+            return new ResponseEntity<>(repository.save(_test), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
