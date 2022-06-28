bd20bea30ba34c66242f284d6854b8b3a621fe96
services <services@apiplatform.io>
2022-06-28 19:47:55 +0530

    testing

---
+++ apicollection/src/test/java/io/apiplatform/v1/apicollection/ApicollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.apiplatform.v1.apicollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
