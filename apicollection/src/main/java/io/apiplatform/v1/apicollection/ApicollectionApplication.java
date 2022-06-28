164028a28eb76598a48916c7edef87543d37a0ed
services <services@apiplatform.io>
2022-06-28 19:48:00 +0530

    testing

---
+++ apicollection/src/main/java/io/apiplatform/v1/apicollection/ApicollectionApplication.java
@@ -0,0 +1,13 @@
+package io.apiplatform.v1.apicollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicollectionApplication.class, args);
+	}
+
+}
